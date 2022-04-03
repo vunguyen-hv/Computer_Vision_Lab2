8fee7615d16a0336ac9665d56cee855d6e91ddca
Nguyễn Vân Huy Vũ <59525527+vunguyen001@users.noreply.github.com>
2022-04-03 18:05:11 +0700

    update sobel and prewitt

--- 19120701_19120726_BT02/Project/Project/19120701_19120726_BT02.cpp
+++ 19120701_19120726_BT02/Project/Project/19120701_19120726_BT02.cpp
@@ -7,40 +7,42 @@ using namespace cv;
 
 Mat ProcessConvolution(Mat input, vector<int> kernel) {
 
-	int row = input.rows;
+	int row = input.rows; //get row and col of image input
 	int col = input.cols;
-	int kernelSize = sqrt(kernel.size());
 
-	Mat output(row, col, CV_8UC1);
+	int kernelSize = sqrt(kernel.size()); //get size of mask
 
-	unsigned char* data_Input = (unsigned char*) (input.data);
+	Mat output(row, col, CV_8UC1); //create a image output
+
+	unsigned char* data_Input = (unsigned char*) (input.data);	//point to memory contain data of input and output image
 	unsigned char* data_Output = (unsigned char*)(output.data);
 	
 	for (int row = 0;  row< input.rows; row++)
 		for (int col = 0; col < input.cols; col++)
 		{
-			if (row < kernelSize / 2 || row >= input.rows - kernelSize / 2 ||
+			if (row < kernelSize / 2 || row >= input.rows - kernelSize / 2 ||  //check if the boundary
 				col < kernelSize / 2 || col >= input.cols - kernelSize / 2) 
 
 			{
-				data_Output[output.step * row + col] = 0;
+				data_Output[output.step * row + col] = 0; // set value of boundary = 0 
 				continue;
 			}
 
-			int Sum = 0, pos_Kernel = 0;
+			int Sum = 0, pos_Kernel = 0; 
 
 			for (int row_Kernel = -kernelSize / 2; row_Kernel <= kernelSize / 2; ++row_Kernel) 
 			{
 				for (int col_Kernel = -kernelSize / 2; col_Kernel <= kernelSize / 2; ++col_Kernel) 
 				{
-					Sum += kernel[pos_Kernel] * data_Input[input.step * (row + row_Kernel) + col + col_Kernel];
+					Sum += kernel[pos_Kernel] * data_Input[input.step * (row + row_Kernel) + col + col_Kernel];  //convolution input image with mask is kernel parameters 
 					pos_Kernel++;
 				}
 			}
 
 			if (Sum < 0) Sum = 0;
-			else if (Sum > 255) Sum = 255;
-			data_Output[output.step * row + col] = Sum;
+			else if (Sum > 255) Sum = 255;	//set color value in [0, 255]
+
+			data_Output[output.step * row + col] = Sum; //assign the results found above to the output
 
 		}
 	return output;
@@ -49,33 +51,34 @@ Mat ProcessConvolution(Mat input, vector<int> kernel) {
 
 
 int detectBySobel(Mat src, Mat dst) {
-	vector<int> S = { 1,0,-1,2,0,-2,1,0,-1 };
-	vector<int> S1 = { 1 ,2 ,1, 0 ,0,0, -1, -2,-1 };
+	vector<int> S = { 1,0,-1,2,0,-2,1,0,-1 }; //mask with X direction  
+	vector<int> S1 = { 1 ,2 ,1, 0 ,0,0, -1, -2,-1 };//mask with Y direction 
 
 	
-	Mat X_direction= ProcessConvolution(src, S);
-	Mat Y_direction = ProcessConvolution(src, S1);
+	Mat X_direction= ProcessConvolution(src, S);	//convolution with X direction
+	Mat Y_direction = ProcessConvolution(src, S1);	//convolution with Y direction
 
-	namedWindow("Display window X", WINDOW_AUTOSIZE); 
-	imshow("Display window X", X_direction);
+	namedWindow("X direction", WINDOW_GUI_NORMAL);//display image
+	imshow("X direction", X_direction);
 
-	namedWindow("Display window Y", WINDOW_AUTOSIZE);
-	imshow("Display window Y", Y_direction);
+	namedWindow("Y direction", WINDOW_GUI_NORMAL);
+	imshow("Y direction", Y_direction);
 
-	unsigned char* X_data = (unsigned char*)X_direction.data;
+	unsigned char* X_data = (unsigned char*)X_direction.data; //point to memory contain data of X and Y direction and destination image
 	unsigned char* Y_data = (unsigned char*)Y_direction.data;
 	unsigned char* destination = (unsigned char*)dst.data;
-	int row = src.rows;
+
+	int row = src.rows;	//get row and col of src image
 	int col = src.cols;
 
 	for (int i = 0; i < row * col; i++)
 	{
-		destination[0] = MIN(sqrt(X_data[0] * X_data[0] + Y_data[0] * Y_data[0]), 255);
+		destination[0] = MIN(sqrt(X_data[0] * X_data[0] + Y_data[0] * Y_data[0]), 255);  //calculate gradient XY_direction 
 		X_data++, Y_data++, destination++;
 	}
 
-	namedWindow("Display window XY", WINDOW_AUTOSIZE);
-	imshow("Display window XY", dst);
+	namedWindow("XY direction" , WINDOW_GUI_NORMAL);
+	imshow("XY direction", dst);
 
 	waitKey(0);
 	return 1;
@@ -85,20 +88,20 @@ int detectBySobel(Mat src, Mat dst) {
 
 
 int detectByPrewitt(Mat src, Mat dst) {
-	vector<int> S = { 1,0,-1,1,0,-1,1,0,-1 };
-	vector<int> S1 = { 1 ,1 ,1, 0 ,0 , 0, -1, -1,-1 };
+	vector<int> S = { 1,0,-1,1,0,-1,1,0,-1 };  //mask with X direction  
+	vector<int> S1 = { 1 ,1 ,1, 0 ,0 , 0, -1, -1,-1 };//mask with Y direction 
 
 
-	Mat X_direction = ProcessConvolution(src, S);
-	Mat Y_direction = ProcessConvolution(src, S1);
+	Mat X_direction = ProcessConvolution(src, S);//convolution with X direction
+	Mat Y_direction = ProcessConvolution(src, S1);//convolution with Y direction
 
-	namedWindow("Display window X_1", WINDOW_AUTOSIZE);
-	imshow("Display window X_1", X_direction);
+	namedWindow("X direction", WINDOW_GUI_NORMAL);//display image
+	imshow("X direction", X_direction);
 
-	namedWindow("Display window Y_1", WINDOW_AUTOSIZE);
-	imshow("Display window Y_1", Y_direction);
+	namedWindow("Y direction", WINDOW_GUI_NORMAL);
+	imshow("Y direction", Y_direction);
 
-	unsigned char* X_data = (unsigned char*)X_direction.data;
+	unsigned char* X_data = (unsigned char*)X_direction.data; //point to memory contain data of X and Y direction and destination image
 	unsigned char* Y_data = (unsigned char*)Y_direction.data;
 	unsigned char* destination = (unsigned char*)dst.data;
 	int row = src.rows;
@@ -106,12 +109,12 @@ int detectByPrewitt(Mat src, Mat dst) {
 
 	for (int i = 0; i < row * col; i++)
 	{
-		destination[0] = MIN(sqrt(X_data[0] * X_data[0] + Y_data[0] * Y_data[0]), 255);
+		destination[0] = MIN(sqrt(X_data[0] * X_data[0] + Y_data[0] * Y_data[0]), 255); //calculate gradient XY_direction 
 		X_data++, Y_data++, destination++;
 	}
 
-	namedWindow("Display window XY_1", WINDOW_AUTOSIZE);
-	imshow("Display window XY_1", dst);
+	namedWindow("XY direction", WINDOW_GUI_NORMAL);
+	imshow("XY direction", dst);
 
 	waitKey(0);
 	return 1;
@@ -132,6 +135,7 @@ int main(int argc, char** argv)
 		if (strcmp(argv[2], "Sobel") == 0) {
 			Mat dst(src.rows, src.cols, CV_8UC1);
 			detectBySobel(src, dst);
+			
 		}
 		else if (strcmp(argv[2], "Prewitt") == 0) {
 			Mat dst(src.rows, src.cols, CV_8UC1);
